bde8355606eca7fa8f8f65c8b7fa421733c839bf
Rachit Agrawal <111473259+Rancho-rachit@users.noreply.github.com>
2024-03-26 09:06:36 +0530

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# DESCRIPTION
+# STORES THE OUTPUT OF COMMAND IN A FILE
+
 if [ "$#" -ne 2 ]; then
         echo -e "\e[0;33mUsage: $0 <command> <output_file>\e[0m"
         exit 1
-
 fi
 
 command=$1
